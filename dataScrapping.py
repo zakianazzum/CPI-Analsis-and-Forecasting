d6996c9ff570a7ce0a49a594e35680fad0d5c997
zakianazzum <zakianazzumgodhuli@gmail.com>
2024-10-01 17:44:01 +0600

    2008 Trend Analysis.

--- dataScrapping.py
+++ dataScrapping.py
@@ -3,5 +3,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+from fredapi import Fred
+
+fred_api_key = os.getenv('API_KEY')
+
+# Creating the Fred Object
+fred = Fred(fred_api_key)
+
+cpi_search = fred.search('Consumer Price Index for All Urban Consumers: All Items in U.S. City Average')
 
 
+
+
+#%%
